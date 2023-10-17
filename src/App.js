6d7a355c412a9507245f551bdf11c45198e7c52b
Vishal Chauhan <vishal.chauhan@digimantra.com>
2023-10-17 17:05:06 +0530

    Worked

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import react from "react";
+import Header from "./components/Layout/Header";
+import { Fragment } from "react";
+import Meals from "./components/Meals/Meals";
+function App() {
+  return (
+    <div>
+      <Fragment>
+        <Header />
+        <main>
+          <Meals />
+        </main>
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cart/CartIcon.js
@@ -0,0 +1,9 @@
+import { BiSolidCartAlt } from "react-icons/bi";
+
+const CartIcon = () => {
+  return (
+    <BiSolidCartAlt />
+  );
+};
+
+export default CartIcon;
